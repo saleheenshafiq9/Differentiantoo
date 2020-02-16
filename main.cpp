9a83b6c56c09f0f96573a6e95d0957ed567b0849
saleheenshafiq9 <48786700+saleheenshafiq9@users.noreply.github.com>
2020-02-15 20:48:10 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+using namespace std;
+
+char input[100];
+
+int main()
+{
+    cin>>input;
+    makeItParts(input);
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+#include<sstream>
+#include <cstdlib>
+#include<stdlib.h>
+#include<stdio.h>
+
+
+
+void makeItParts(char[]);
+void differentiateParts(char[],char);
