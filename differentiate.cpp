c2e1987e2ef5c8bee17351343ddd55f38f1f7de8
saleheenshafiq9 <48786700+saleheenshafiq9@users.noreply.github.com>
2021-07-28 07:43:37 +0600

    yaa

--- differentiate.cpp
+++ differentiate.cpp
@@ -6,25 +6,29 @@ char universal_sign = '\0';
 
 void differentiate(int coeff, char variable, int power, char sign) {
 
-
-    if(sign=='f') {
-        universal_sign = '\0';
-//        cout<<"check";
-    }
     if(coeff==1001)
         return;
 
     int new_coeff = coeff*power;
     int new_power = power-1;
 
+    int sign_remover = 0;
+
     if(universal_sign=='+' || universal_sign=='-') {
         if(new_coeff>0)
             cout<<" "<<universal_sign<<" ";
         else if(new_coeff<0) {
-            if(universal_sign=='+')
-                cout<<" "<<"-"<<" ";
-            else
-                cout<<" "<<"+"<<" ";
+            if(sign_remover==0){
+                sign_remover++;
+                if(universal_sign=='+')
+                    cout<<" "<<"-"<<" ";
+            }
+            else {
+                if(universal_sign=='+')
+                    cout<<" "<<"-"<<" ";
+                else
+                    cout<<" "<<"+"<<" ";
+            }
         }
         if(new_coeff!=1)
             cout<<abs(new_coeff);
@@ -45,6 +49,10 @@ void differentiate(int coeff, char variable, int power, char sign) {
         cout<<"^"<<new_power;
     universal_sign = sign;
 
+        if(sign=='f') {
+        universal_sign = '\0';
+//        cout<<"check";
+    }
 
 }
 
